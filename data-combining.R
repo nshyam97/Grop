e73f799b96a56f4a1e993aa3256263e410e84273
stanlypalathingal <b9058957@newcastle.ac.uk>
2020-03-04 16:45:01 +0000

    first commit

--- data-combining.R
+++ data-combining.R
@@ -1,5 +1,13 @@
-#Start of file
+## Load the dataset
+setwd("C:/Users/HP/OneDrive - Newcastle University/Group project")
+library(magrittr)
+# load the files
+A= read.csv("code/A.csv")
+B= read.csv("code/B.csv")
+P= read.csv("code/Power.csv")
 
+A$time = format(as.POSIXct(A$Time,format ="%d/%m/%Y %H:%M:%S"),"%H:%M:%S")
+ 
+A$Date <- format(as.Date(A$Time, format ="%d/%m/%Y"), "%d/%m/%Y")
 
-# Change on stack 1
-
+View(A)
