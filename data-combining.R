da1434c58a491cda132fe8610d7a059a7d80426f
qayyim24 <qayyim@gmail.com>
2020-03-29 16:02:38 +0100

    Added "system_data"
    Added system data files and combined them.

--- data-combining.R
+++ data-combining.R
@@ -18,6 +18,25 @@ A_df = A_K2 %>% ldply(read.csv) %>% unique() %>% mutate(Time = as.POSIXct(Time, 
 B_df = B_K2 %>% ldply(read.csv) %>% unique() %>% mutate(Time = as.POSIXct(Time, format="%d/%m/%Y %H:%M:%S"))
 Power_df = Power %>% ldply(read.csv) %>% unique() %>% mutate(Time = as.POSIXct(Time, format="%d/%m/%Y %H:%M:%S"))
 
+mydir="system_data" # Directory for system data
+
+# List system data files
+P_demand = list.files(path = mydir, pattern = "*.CSV", full.names = TRUE)
+
+P_demand = P_demand %>% 
+  ldply(read.csv) %>% 
+  mutate(timestamp= paste(Date,Time))%>%
+  mutate(timestamp= as.POSIXct(timestamp, format="%Y/%m/%d %H:%M:%S"))
+
+# Remove columns with legacy data
+P_demand = P_demand[,c(3:12,21)]
+
+# Reorder Columns so "timestamp" is now first
+P_demand = P_demand[ , c(ncol(P_demand), 1:(ncol(P_demand)-1))]
+
+# rename "timestamp" to "Time"
+names(P_demand)[1] <- "Time"
+
 # Save combine data as new csv
 write.csv(A_df, "A.csv", row.names = FALSE)
 write.csv(B_df, "B.csv", row.names = FALSE)
